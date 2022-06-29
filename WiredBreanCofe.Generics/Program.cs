a60349ff9f3061db57cf30e23edd1abc48302e75
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2022-06-29 08:45:12 +0200

    Refact DoubleSimpleStack

--- WiredBreanCofe.Generics/Program.cs
+++ WiredBreanCofe.Generics/Program.cs
@@ -7,18 +7,30 @@ namespace WiredBreanCofe.Generics
     {
         static void Main(string[] args)
         {
+            DoubleSimpleStack();
+
+            Console.ReadLine();
+        }
+
+        private static void DoubleSimpleStack()
+        {
             SimpleStack stack = new SimpleStack();
             stack.Push(1.2);
             stack.Push(2.8);
             stack.Push(3.0);
 
+            double sum = 0;
+
             while (stack.Count() > 0)
             {
-                var  item = stack.Pop();
+                var item = stack.Pop();
+
+                sum += item;
+
                 Console.WriteLine($"Count {stack.Count()}, Item {item}");
             }
 
-            Console.ReadLine();
+            Console.WriteLine($"Sum {sum}");
         }
     }
 }
