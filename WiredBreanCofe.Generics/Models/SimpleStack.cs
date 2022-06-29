713d078b9a2396067a7c17d39a897cccf979c63d
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2022-06-29 08:22:36 +0200

    Add project files.

---
+++ WiredBreanCofe.Generics/Models/SimpleStack.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiredBreanCofe.Generics.Models
+{
+    public class SimpleStack
+    {
+        private readonly double[] _items;
+        private int index = -1;
+
+        public SimpleStack()
+        {
+            _items = new double[10];
+        }
+
+        public void push(double item) => _items[++ index] = item;
+        public double pop() => _items[index--];
+    
+    }
+}
